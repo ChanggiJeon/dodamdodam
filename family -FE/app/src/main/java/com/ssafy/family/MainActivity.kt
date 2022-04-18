b9ceb12ee186b027b24e2d7149df84c8b17688af
SSAFY <likppi100@naver.com>
2022-04-18 16:37:05 +0900

    FE/feature/main 바텀 네비게이션 : MainActivity 바텀네비게이션 화면구성

--- family -FE/app/src/main/java/com/ssafy/family/MainActivity.kt
+++ family -FE/app/src/main/java/com/ssafy/family/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ssafy.family
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ssafy.family.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainBottomNavigation.setupWithNavController(findNavController(R.id.main_fragment))
     }
 }
