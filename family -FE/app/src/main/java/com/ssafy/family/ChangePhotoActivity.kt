93218e1703e7b5dc869bfaa51ebf9e304ada6c42
SuhyungLee <eel0511@gmail.com>
2022-04-19 16:04:31 +0900

    [S06P31D203-17] Feat/FE/프로필정보변경: ChangePhotoActivity 화면구성

---
+++ family -FE/app/src/main/java/com/ssafy/family/ChangePhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.ssafy.family
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_photo)
+    }
+}
