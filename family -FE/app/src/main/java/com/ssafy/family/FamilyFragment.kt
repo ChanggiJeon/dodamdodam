f0521ad1dc7aa2d026dd0743f096a2b4a1727262
SSAFY <likppi100@naver.com>
2022-04-20 13:54:27 +0900

    [S06P31D203-44] FE/feature/알림 : "알림 보내기" 또는 profile image 클릭 시 화면 아래로 열리고 메세지 버튼 나오는 UI 구현     (사랑해, 보고싶어, 감사해요, 이따 봐용~, 오늘도 화이팅, 밥 먹었어? 등 20개 이상 입력)      / 6개씩 슬라이딩하여 보여주기 / 최근 사용한 메시지 최전방으로 이동)

--- family -FE/app/src/main/java/com/ssafy/family/FamilyFragment.kt
+++ family -FE/app/src/main/java/com/ssafy/family/FamilyFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
+import com.ssafy.family.databinding.FragmentFamilyBinding
+import com.ssafy.family.databinding.FragmentHomeBinding
 
 class FamilyFragment : Fragment() {
+
+    private lateinit var binding: FragmentFamilyBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,13 @@ class FamilyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_family, container, false)
+        binding = FragmentFamilyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = StatusAdapter(requireActivity())
+        binding.statusRecyclerView.adapter = adapter
     }
 }
--- family -FE/app/src/main/java/com/ssafy/family/MainActivity.kt
+++ family -FE/app/src/main/java/com/ssafy/family/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.ssafy.family.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
