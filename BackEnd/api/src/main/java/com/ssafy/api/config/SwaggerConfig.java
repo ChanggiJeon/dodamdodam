e06f54c627928cdfba0d7c56c4de2f6754968538
전창기 <wjs1724@naver.com>
2022-04-26 22:47:31 +0900

    Feat/Server : finish to check run

--- BackEnd/api/src/main/java/com/ssafy/api/config/SwaggerConfig.java
+++ BackEnd/api/src/main/java/com/ssafy/api/config/SwaggerConfig.java
@@ -3,32 +3,29 @@
  */
 package com.ssafy.api.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-
-
 @Configuration
 public class SwaggerConfig {
-    private String version;
-    private String title;
 
     @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("SpringShop API")
-                        .description("Spring shop sample application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://dodamdodam.com")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("SpringShop Wiki Documentation")
-                        .url("https://springshop.wiki.github.org/docs"));
-    }
+    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+        Info info = new Info().title("Demo API").version(appVersion)
+                .description("Spring Boot를 이용한 Demo 웹 애플리케이션 API입니다.")
+                .termsOfService("http://swagger.io/terms/")
+                .contact(new Contact().name("hanjibung").url("https://blog.jiniworld.me/").email("wjs1724@naver.com"))
+                .license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
 
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
 
 }
