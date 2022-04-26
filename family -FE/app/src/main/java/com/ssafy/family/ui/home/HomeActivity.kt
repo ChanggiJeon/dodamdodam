624cc33119a5756e0cb7267c87052529bb34cee9
SuhyungLee <eel0511@gmail.com>
2022-04-26 15:58:34 +0900

    Feature/FE/FCM : FCM 올배 배지 변경과 바텀네이게이션 연결 재정의

--- family -FE/app/src/main/java/com/ssafy/family/ui/home/HomeActivity.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/home/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.ssafy.family.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
 import com.ssafy.family.R
 import com.ssafy.family.databinding.ActivityHomeBinding
 import com.ssafy.family.ui.schedule.AddScheduleFragment
@@ -12,8 +14,10 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.d("ddddddd", "onCreate: "+FirebaseMessaging.getInstance().token.result)
         supportFragmentManager.beginTransaction()
             .replace(R.id.home_frame, LoginFragment())
             .commit()
+
     }
 }
