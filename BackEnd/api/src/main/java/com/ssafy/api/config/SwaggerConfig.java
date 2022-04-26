4ba4944659d8d0cecdd4a4df30089cd0222add1d
윤재성 <wotjd4725@gmail.com>
2022-04-26 16:45:05 +0900

    Fix: go back to previous version

--- BackEnd/api/src/main/java/com/ssafy/api/config/SwaggerConfig.java
+++ BackEnd/api/src/main/java/com/ssafy/api/config/SwaggerConfig.java
@@ -5,19 +5,21 @@ package com.ssafy.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
     private String version;
     private String title;
