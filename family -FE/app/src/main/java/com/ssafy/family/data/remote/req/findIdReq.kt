49263d68847d71e28083587542fa4974f867e3aa
SuhyungLee <eel0511@gmail.com>
2022-04-28 11:10:15 +0900

    [S06P31D203-119] Feature/FE/계정찾기 : 아이디 찾기 Res, Req 구현

---
+++ family -FE/app/src/main/java/com/ssafy/family/data/remote/req/findIdReq.kt
@@ -0,0 +1,16 @@
+package com.ssafy.family.data.remote.req
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class findIdReq(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("birthday")
+    val birthday: String,
+    @SerializedName("familyCode")
+    val familyCode: String,
+) : Parcelable
+
