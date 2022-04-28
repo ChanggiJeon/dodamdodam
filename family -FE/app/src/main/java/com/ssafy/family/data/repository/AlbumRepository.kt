1a1c256c20f856cc872c8c4e4cd063bcf0493e46
SuhyungLee <eel0511@gmail.com>
2022-04-28 18:00:37 +0900

    Fix/FE/Album : Hilt 적용

--- family -FE/app/src/main/java/com/ssafy/family/data/repository/AlbumRepository.kt
+++ family -FE/app/src/main/java/com/ssafy/family/data/repository/AlbumRepository.kt
@@ -1,9 +1,9 @@
 package com.ssafy.family.data.repository
 
-import com.bumptech.glide.load.engine.Resource
+
 import com.ssafy.family.data.remote.res.AlbumRes
-import com.ssafy.family.data.remote.res.AllAlbum
+import com.ssafy.family.util.Resource
 
 interface AlbumRepository {
-    suspend fun findAllAlbum():Resource<AlbumRes>
+    suspend fun findAllAlbum(): Resource<AlbumRes>
 }
