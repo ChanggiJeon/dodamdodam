d19d7c7696d4985f00d892c12d6011125450f1c2
SuhyungLee <eel0511@gmail.com>
2022-05-02 10:56:03 +0900

    Feat/FE/Album : 앨범 화면 재구성

--- family -FE/app/src/main/java/com/ssafy/family/ui/main/bottomFragment/AlbumFragment.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/main/bottomFragment/AlbumFragment.kt
@@ -1,22 +1,15 @@
 package com.ssafy.family.ui.main.bottomFragment
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
-import com.google.firebase.messaging.FirebaseMessaging
-import com.ssafy.family.data.remote.req.AddFcmReq
+import com.ssafy.family.data.remote.res.*
 import com.ssafy.family.ui.Adapter.AlbumMonthAdapter
 import com.ssafy.family.databinding.FragmentAlbumBinding
-import com.ssafy.family.ui.Adapter.AlbumAdapter
-import com.ssafy.family.ui.home.LoginViewModel
-import com.ssafy.family.ui.main.MainActivity
-import com.ssafy.family.util.LoginUtil
 import com.ssafy.family.util.Status
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,8 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class AlbumFragment : Fragment() {
 
     private lateinit var binding: FragmentAlbumBinding
-    private lateinit var albumMonthAdapter:AlbumMonthAdapter
-    private lateinit var albumAdapter: AlbumAdapter
+    private lateinit var albumMonthAdapter: AlbumMonthAdapter
     private val albumViewModel by activityViewModels<AlbumViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,24 +37,35 @@ class AlbumFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initView()
     }
-    private fun initView(){
+
+    private fun initView() {
         findAllAlbum()
         albumMonthAdapter = AlbumMonthAdapter(requireActivity())
-        albumAdapter = AlbumAdapter(requireContext())
         binding.monthRecyclerView.adapter = albumMonthAdapter
-        albumViewModel.allAlbumRequestLiveData.observe(requireActivity()){
+        albumViewModel.allAlbumRequestLiveData.observe(requireActivity()) {
             when (it.status) {
                 Status.SUCCESS -> {
-                    it.data!!.dataSet!!.forEach {
-                        albumMonthAdapter.AlbumAdapter.datas.add(it)
-                        albumAdapter.notifyDataSetChanged()
-                        albumMonthAdapter.notifyDataSetChanged()
-                    }
-
+                    albumMonthAdapter.datas = (it.data!!.dataSet as MutableList<AllAlbum>?)!!
+                    albumMonthAdapter.notifyDataSetChanged()
                     dismissLoading()
                 }
                 Status.ERROR -> {
-                    Toast.makeText(requireActivity(), it.message?:"서버 에러", Toast.LENGTH_SHORT).show()
+                    //테스트용
+                    val taglist = mutableListOf<HashTag>()
+                    taglist.add(HashTag("#해시"))
+                    taglist.add(HashTag("#해시"))
+                    val albumlist = mutableListOf<Album>()
+                    albumlist.add(Album(0, mutableListOf(),"2022년 5월 2일"))
+                    val temppicture = Picture(1,albumlist,"d","https://cdn.pixabay.com/photo/2019/08/01/12/36/illustration-4377408_960_720.png",true)
+                    val allalbumlist = mutableListOf<AllAlbum>()
+                    allalbumlist.add(AllAlbum(taglist,temppicture))
+                    allalbumlist.add(AllAlbum(taglist,temppicture))
+                    allalbumlist.add(AllAlbum(taglist,temppicture))
+                    albumMonthAdapter.datas= (allalbumlist)
+                    albumMonthAdapter.notifyDataSetChanged()
+                    //테스트용 끝
+                    Toast.makeText(requireActivity(), it.message ?: "서버 에러", Toast.LENGTH_SHORT)
+                        .show()
                     dismissLoading()
                 }
                 Status.LOADING -> {
@@ -71,9 +74,11 @@ class AlbumFragment : Fragment() {
             }
         }
     }
-    private fun findAllAlbum(){
+
+    private fun findAllAlbum() {
         albumViewModel.findAllAlbum()
     }
+
     private fun setLoading() {
         binding.progressBarLoginFLoading.visibility = View.VISIBLE
     }
