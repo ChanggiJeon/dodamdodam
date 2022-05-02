0b65ebbdcee0dfd1f36418b2c618a4cf40cded65
John Kim <dhkdhd123@naver.com>
2022-05-02 16:48:42 +0900

    [S06P31D203-132] Feat/FE/Add : FamilyActivity

--- family -FE/app/src/main/java/com/ssafy/family/ui/startsetting/StartSettingActivity.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/startsetting/StartSettingActivity.kt
@@ -3,10 +3,20 @@ package com.ssafy.family.ui.startsetting
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ssafy.family.R
+import com.ssafy.family.databinding.ActivityStartSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StartSettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartSettingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_setting)
+        binding = ActivityStartSettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    supportFragmentManager.beginTransaction()
+        .replace(R.id.fragment_in_start_setting, AskFamilyCodeFragment())
+        .commit()
     }
 }
