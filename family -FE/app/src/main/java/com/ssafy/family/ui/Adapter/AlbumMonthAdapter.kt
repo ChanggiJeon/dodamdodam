eb0e94028ca48bf6323ad2365638a6257f2e6fd6
SuhyungLee <eel0511@gmail.com>
2022-05-02 11:21:30 +0900

    Feat/FE/Album : 앨범프래그먼트 화면 binding과 클릭리스너추가

--- family -FE/app/src/main/java/com/ssafy/family/ui/Adapter/AlbumMonthAdapter.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/Adapter/AlbumMonthAdapter.kt
@@ -9,7 +9,6 @@ import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ssafy.family.R
-import com.ssafy.family.data.remote.res.AlbumRes
 import com.ssafy.family.data.remote.res.AllAlbum
 import com.ssafy.family.databinding.ItemMonthAlbumBinding
 
@@ -18,6 +17,7 @@ class AlbumMonthAdapter(private val context: Context) :
     RecyclerView.Adapter<AlbumMonthAdapter.ViewHolder>() {
 
     var datas = mutableListOf<AllAlbum>()
+    lateinit var itemClickListener: ItemClickListener
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemMonthAlbumBinding.inflate(inflater, parent, false)
@@ -37,16 +37,21 @@ class AlbumMonthAdapter(private val context: Context) :
 
         private val albumtag: TextView = itemView.findViewById(R.id.album_main_hashtag)
         private val albumimg: ImageView = itemView.findViewById(R.id.album_main_img)
-        private val albumdate:TextView= itemView.findViewById(R.id.album_main_date)
+        private val albumdate: TextView = itemView.findViewById(R.id.album_main_date)
         fun bind(item: AllAlbum) {
-            albumdate.text=item.mainPicture.album[0].date
+            albumdate.text = item.mainPicture.album[0].date
             Glide.with(itemView).load(item.mainPicture.path_name).into(albumimg)
             var alltag = ""
-            item.hashTags.forEach { alltag+=it.text }
-            albumtag.text=alltag
-
+            item.hashTags.forEach { alltag += it.text }
+            albumtag.text = alltag
+            itemView.setOnClickListener {
+                itemClickListener.onClick(item)
+            }
         }
     }
 
+    interface ItemClickListener {
+        fun onClick(allAlbum: AllAlbum)
+    }
 
 }
--- family -FE/app/src/main/java/com/ssafy/family/ui/Adapter/DetailAlbumPhotoAdapter.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/Adapter/DetailAlbumPhotoAdapter.kt
@@ -7,15 +7,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.family.R
 import com.ssafy.family.databinding.AlbumPhotoListBinding
 
-class DetailAlbumPhotoAdapter(private val context: Context) : RecyclerView.Adapter<DetailAlbumPhotoAdapter.ViewHolder>() {
+class DetailAlbumPhotoAdapter(private val context: Context) :
+    RecyclerView.Adapter<DetailAlbumPhotoAdapter.ViewHolder>() {
 
-    private var datas = mutableListOf<Int>(1,2)
+    private var datas = mutableListOf<Int>(1, 2)
     lateinit var itemClickListener: ItemClickListener
-    inner class ViewHolder(val binding: AlbumPhotoListBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(){
+
+    inner class ViewHolder(val binding: AlbumPhotoListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind() {
             binding.albumPhotoListImg.setImageResource(R.drawable.aa)
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         //val view = LayoutInflater.from(context).inflate(R.layout.item_family_status,parent,false)
         val inflater = LayoutInflater.from(parent.context)
@@ -28,6 +32,7 @@ class DetailAlbumPhotoAdapter(private val context: Context) : RecyclerView.Adapt
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind()
     }
+
     interface ItemClickListener {
         fun onClick()
     }
