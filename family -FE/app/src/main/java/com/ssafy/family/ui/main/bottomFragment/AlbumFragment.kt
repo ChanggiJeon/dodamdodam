21cf62b667392c8e70e3c614a8abe6b6bb744a6a
SuhyungLee <eel0511@gmail.com>
2022-05-02 11:21:57 +0900

    Feat/FE/Album : 앨범전체 화면 테스트완료

--- family -FE/app/src/main/java/com/ssafy/family/ui/main/bottomFragment/AlbumFragment.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/main/bottomFragment/AlbumFragment.kt
@@ -1,25 +1,41 @@
 package com.ssafy.family.ui.main.bottomFragment
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.ssafy.family.data.remote.res.*
-import com.ssafy.family.ui.Adapter.AlbumMonthAdapter
+import com.ssafy.family.data.remote.res.Album
+import com.ssafy.family.data.remote.res.AllAlbum
+import com.ssafy.family.data.remote.res.HashTag
+import com.ssafy.family.data.remote.res.Picture
 import com.ssafy.family.databinding.FragmentAlbumBinding
+import com.ssafy.family.ui.Adapter.AlbumMonthAdapter
+import com.ssafy.family.ui.album.AlbumActivity
 import com.ssafy.family.util.Status
 import dagger.hilt.android.AndroidEntryPoint
 
 // mainactivity - albumfragment : 월 리스트 (2021.06, 2021,07 ...)
 @AndroidEntryPoint
 class AlbumFragment : Fragment() {
+    companion object {
+        val FRIEND_INFO = "friendInfo"
+    }
 
     private lateinit var binding: FragmentAlbumBinding
     private lateinit var albumMonthAdapter: AlbumMonthAdapter
     private val albumViewModel by activityViewModels<AlbumViewModel>()
+    private val itemClickListener = object : AlbumMonthAdapter.ItemClickListener {
+        override fun onClick(allAlbum: AllAlbum) {
+            val intent = Intent(requireActivity(), AlbumActivity::class.java)
+            intent.putExtra(FRIEND_INFO, allAlbum)
+            startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,7 +56,9 @@ class AlbumFragment : Fragment() {
 
     private fun initView() {
         findAllAlbum()
-        albumMonthAdapter = AlbumMonthAdapter(requireActivity())
+        albumMonthAdapter = AlbumMonthAdapter(requireActivity()).apply {
+            itemClickListener = this@AlbumFragment.itemClickListener
+        }
         binding.monthRecyclerView.adapter = albumMonthAdapter
         albumViewModel.allAlbumRequestLiveData.observe(requireActivity()) {
             when (it.status) {
@@ -55,13 +73,19 @@ class AlbumFragment : Fragment() {
                     taglist.add(HashTag("#해시"))
                     taglist.add(HashTag("#해시"))
                     val albumlist = mutableListOf<Album>()
-                    albumlist.add(Album(0, mutableListOf(),"2022년 5월 2일"))
-                    val temppicture = Picture(1,albumlist,"d","https://cdn.pixabay.com/photo/2019/08/01/12/36/illustration-4377408_960_720.png",true)
+                    albumlist.add(Album(0, mutableListOf(), "2022년 5월 2일"))
+                    val temppicture = Picture(
+                        1,
+                        albumlist,
+                        "d",
+                        "https://cdn.pixabay.com/photo/2019/08/01/12/36/illustration-4377408_960_720.png",
+                        true
+                    )
                     val allalbumlist = mutableListOf<AllAlbum>()
-                    allalbumlist.add(AllAlbum(taglist,temppicture))
-                    allalbumlist.add(AllAlbum(taglist,temppicture))
-                    allalbumlist.add(AllAlbum(taglist,temppicture))
-                    albumMonthAdapter.datas= (allalbumlist)
+                    allalbumlist.add(AllAlbum(taglist, temppicture))
+                    allalbumlist.add(AllAlbum(taglist, temppicture))
+                    allalbumlist.add(AllAlbum(taglist, temppicture))
+                    albumMonthAdapter.datas = (allalbumlist)
                     albumMonthAdapter.notifyDataSetChanged()
                     //테스트용 끝
                     Toast.makeText(requireActivity(), it.message ?: "서버 에러", Toast.LENGTH_SHORT)
