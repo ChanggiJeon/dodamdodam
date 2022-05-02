c1d9fef9db83a92a1500068a895d9b009d356a3d
SuhyungLee <eel0511@gmail.com>
2022-05-02 11:42:48 +0900

    Feat/FE/Album : 앨범 상세정보 필요한 Repo생성

--- family -FE/app/src/main/java/com/ssafy/family/data/repository/AlbumRepository.kt
+++ family -FE/app/src/main/java/com/ssafy/family/data/repository/AlbumRepository.kt
@@ -1,9 +1,11 @@
 package com.ssafy.family.data.repository
 
 
+import com.ssafy.family.data.remote.res.AlbumDetailRes
 import com.ssafy.family.data.remote.res.AlbumRes
 import com.ssafy.family.util.Resource
 
 interface AlbumRepository {
     suspend fun findAllAlbum(): Resource<AlbumRes>
+    suspend fun detailAlbum(albumId:Int):Resource<AlbumDetailRes>
 }
--- family -FE/app/src/main/java/com/ssafy/family/data/repository/AlbumRepositoryImpl.kt
+++ family -FE/app/src/main/java/com/ssafy/family/data/repository/AlbumRepositoryImpl.kt
@@ -2,16 +2,17 @@ package com.ssafy.family.data.repository
 
 
 import com.ssafy.family.data.remote.api.AlbumAPI
+import com.ssafy.family.data.remote.res.AlbumDetailRes
 import com.ssafy.family.data.remote.res.AlbumRes
 import com.ssafy.family.util.Resource
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import java.lang.Exception
 
-class AlbumRepositoryImpl(private val api: AlbumAPI,
-                          private val ioDispatcher: CoroutineDispatcher,
-                          private val mainDispatcher: CoroutineDispatcher
-) :AlbumRepository {
+class AlbumRepositoryImpl(
+    private val api: AlbumAPI,
+    private val ioDispatcher: CoroutineDispatcher,
+    private val mainDispatcher: CoroutineDispatcher
+) : AlbumRepository {
     override suspend fun findAllAlbum(): Resource<AlbumRes> = withContext(ioDispatcher) {
         try {
             val response = api.allAlbum()
@@ -23,7 +24,26 @@ class AlbumRepositoryImpl(private val api: AlbumAPI,
                     Resource.expired(response.body()!!)
                 }
                 else -> {
-                    Resource.error(null,response.message())
+                    Resource.error(null, response.message())
+                }
+            }
+        } catch (e: Exception) {
+            Resource.error(null, "서버와 연결오류")
+        }
+    }
+
+    override suspend fun detailAlbum(albumId: Int): Resource<AlbumDetailRes> = withContext(ioDispatcher) {
+        try {
+            val response = api.detailAlbum(albumId)
+            when {
+                response.isSuccessful -> {
+                    Resource.success(response.body()!!)
+                }
+                response.code() == 403 -> {
+                    Resource.expired(response.body()!!)
+                }
+                else -> {
+                    Resource.error(null, response.message())
                 }
             }
         } catch (e: Exception) {
