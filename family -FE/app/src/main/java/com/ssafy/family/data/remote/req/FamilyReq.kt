4d2a3dd8d1af6dd5997be5744f58bf31d1fb95c8
John Kim <dhkdhd123@naver.com>
2022-05-03 14:21:32 +0900

    [S06P31D203-146] Feat/FE/Add : family req res

---
+++ family -FE/app/src/main/java/com/ssafy/family/data/remote/req/FamilyReq.kt
@@ -0,0 +1,17 @@
+package com.ssafy.family.data.remote.req
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class FamilyReq(
+    @SerializedName("role")
+    val role: String,
+    @SerializedName("nickname")
+    val nickname: String,
+    @SerializedName("birthday")
+    val birthday: String,
+    @SerializedName("image")
+    val image: String,
+) : Parcelable
