b4a45505b9f024167327f853c5f5f57a9f95d3d1
SuhyungLee <eel0511@gmail.com>
2022-05-06 15:03:18 +0900

    Feature/FE/앨범수정 : 앨범수정 실행로직

--- family -FE/app/src/main/java/com/ssafy/family/ui/album/AlbumActivity.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/album/AlbumActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.ssafy.family.R
 import com.ssafy.family.data.remote.req.AddAlbumReq
+import com.ssafy.family.data.remote.req.UpdateAlbumReq
 import com.ssafy.family.data.remote.res.AllAlbum
 import com.ssafy.family.databinding.ActivityAlbumBinding
 import com.ssafy.family.ui.main.bottomFragment.AlbumFragment
@@ -66,32 +67,73 @@ class AlbumActivity : AppCompatActivity() {
             } else {
                 binding.albumButtonInclude.root.visibility = View.VISIBLE
             }
+
             if (it == "완료") {
                 binding.albumButtonInclude.button2.setOnClickListener {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.album_frame, AddAlbumFragment())
-                        .commit()
+                    if(detailAlbumViewModel.isUpdate){
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.album_frame, UpdateAlbumFragment())
+                            .commit()
+                    }else{
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.album_frame, AddAlbumFragment())
+                            .commit()
+                    }
+
 
                 }
             }
             if (it == "등록") {
-                Log.d("ddddd", "등록: ")
-                val hashTag =  detailAlbumViewModel.hashTag
-                val date = detailAlbumViewModel.date
-                val mainIndex = detailAlbumViewModel.mainIndex
-                Log.d("ddddd", "onCreate: "+hashTag)
-                Log.d("ddddd", "onCreate: "+date)
-                Log.d("ddddd", "onCreate: "+mainIndex)
-                if(!hashTag.isNullOrEmpty()&&!date.isNullOrEmpty()&&mainIndex!=null){
-                    Log.d("ddddd", "in: ")
-                    detailAlbumViewModel.addAlbum(
-                        AddAlbumReq(
-                            hashTag,
-                            date,
-                            mainIndex
+                binding.albumButtonInclude.button2.setOnClickListener {
+                    Log.d("ddddd", "등록: ")
+                    val hashTag = detailAlbumViewModel.hashTag
+                    val date = detailAlbumViewModel.date
+                    val mainIndex = detailAlbumViewModel.mainIndex
+                    Log.d("ddddd", "onCreate: " + hashTag)
+                    Log.d("ddddd", "onCreate: " + date)
+                    Log.d("ddddd", "onCreate: " + mainIndex)
+                    if (!hashTag.isNullOrEmpty() && !date.isNullOrEmpty() && mainIndex != null) {
+                        Log.d("ddddd", "in: ")
+                        detailAlbumViewModel.addAlbum(
+                            AddAlbumReq(
+                                hashTag,
+                                date,
+                                mainIndex
+                            )
                         )
-                    )
-                    finish()
+                        detailAlbumViewModel.hashTag = arrayListOf()
+                        detailAlbumViewModel.date = ""
+                        detailAlbumViewModel.mainIndex = 0
+                        finish()
+                    }
+                }
+            }
+            if (it == "수정") {
+                binding.albumButtonInclude.button2.setOnClickListener {
+                    Log.d("ddddd", "수정: ")
+                    val hashTag = detailAlbumViewModel.hashTag
+                    val date = detailAlbumViewModel.date
+                    val mainIndex = detailAlbumViewModel.mainIndex
+                    val pictureList = detailAlbumViewModel.pictureIdList
+                    Log.d("ddddd", "onCreate: " + hashTag)
+                    Log.d("ddddd", "onCreate: " + date)
+                    Log.d("ddddd", "onCreate: " + mainIndex)
+                    if (!hashTag.isNullOrEmpty() && !date.isNullOrEmpty() && mainIndex != null) {
+                        Log.d("ddddd", "in: ")
+                        detailAlbumViewModel.updateAlbum(
+                            UpdateAlbumReq(
+                                hashTag,
+                                date,
+                                mainIndex,
+                                pictureList
+                            ),
+                            detailAlbumViewModel.saveAlbumLiveData.value!!.mainPicture.albumId
+                        )
+                        detailAlbumViewModel.hashTag = arrayListOf()
+                        detailAlbumViewModel.date = ""
+                        detailAlbumViewModel.mainIndex = 0
+                        finish()
+                    }
                 }
             }
         }
@@ -110,6 +152,9 @@ class AlbumActivity : AppCompatActivity() {
                     setLoading()
                 }
             }
+        }
+        binding.albumButtonInclude.button2.setOnClickListener {
+
         }
 
     }
