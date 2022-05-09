a8fe7c644e2f91b0d849356170cc0088e0cd5a4e
전창기 <wjs1724@naver.com>
2022-05-09 11:46:33 +0900

    [S06P31D203-245] Feat/BE/Add : FamilyCode, LocalDate Validator

---
+++ BackEnd/dodamdodam/core/src/main/java/com/ssafy/core/validator/LocalDate.java
@@ -0,0 +1,24 @@
+package com.ssafy.core.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = LocalDateValidator.class)
+@Target({FIELD, METHOD})
+@Retention(RUNTIME)
+public @interface LocalDate {
+
+    String message() default "입력하신 날짜가 형식에 맞지 않습니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
