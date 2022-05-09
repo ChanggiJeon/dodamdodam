8dc938998df0959b27d9d5f324457c84141a5754
SuhyungLee <eel0511@gmail.com>
2022-05-09 10:31:49 +0900

    [S06P31D203-265] Feature/FE/Family : 오늘의 미션 ViewModel

---
+++ family -FE/app/src/main/java/com/ssafy/family/ui/main/MainFamilyViewModel.kt
@@ -0,0 +1,24 @@
+package com.ssafy.family.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.family.data.remote.res.MissionRes
+import com.ssafy.family.data.repository.MainFamilyRepository
+import com.ssafy.family.util.Resource
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MainFamilyViewModel @Inject constructor(private val mainFamilyRepository: MainFamilyRepository) :
+    ViewModel() {
+
+        private val _todayMissionRequestLiveData = MutableLiveData<Resource<MissionRes>>()
+        val todayMissionRequestLiveData:LiveData<Resource<MissionRes>>
+        get() = _todayMissionRequestLiveData
+
+    fun getTodayMission() = viewModelScope.launch {
+        _todayMissionRequestLiveData.postValue(Resource.loading(null))
+        _todayMissionRequestLiveData.postValue(mainFamilyRepository.getTodayMission())
+    }
+}
