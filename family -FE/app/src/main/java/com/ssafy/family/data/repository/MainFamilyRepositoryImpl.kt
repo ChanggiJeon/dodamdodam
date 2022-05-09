8a1fda3d27377a23662ed62a05c46f7dd62e2c4a
SuhyungLee <eel0511@gmail.com>
2022-05-09 10:21:20 +0900

    [S06P31D203-265] Feature/FE/Family : 오늘의 미션 repo

--- family -FE/app/src/main/java/com/ssafy/family/data/repository/MainFamilyRepositoryImpl.kt
+++ family -FE/app/src/main/java/com/ssafy/family/data/repository/MainFamilyRepositoryImpl.kt
@@ -4,13 +4,29 @@ import com.ssafy.family.data.remote.api.MainFamilyAPI
 import com.ssafy.family.data.remote.res.MissionRes
 import com.ssafy.family.util.Resource
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 class MainFamilyRepositoryImpl(
     private val api: MainFamilyAPI,
     private val ioDispatcher: CoroutineDispatcher,
     private val mainDispatcher: CoroutineDispatcher
 ) :MainFamilyRepository{
-    override suspend fun getTodayMission(): Resource<MissionRes> {
-        TODO("Not yet implemented")
+    override suspend fun getTodayMission(): Resource<MissionRes> = withContext(ioDispatcher) {
+        try {
+            val response = api.getTodayMission()
+            when {
+                response.isSuccessful -> {
+                    Resource.success(response.body()!!)
+                }
+                response.code() == 403 -> {
+                    Resource.expired(response.body()!!)
+                }
+                else -> {
+                    Resource.error(null, response.message())
+                }
+            }
+        } catch (e: Exception) {
+            Resource.error(null, "서버와 연결오류")
+        }
     }
 }
