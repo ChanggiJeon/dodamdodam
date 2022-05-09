c70b3a569cc6620deb8235dd705a28f06b637e8b
SuhyungLee <eel0511@gmail.com>
2022-05-09 11:10:40 +0900

    [S06P31D203-265] Feature/FE/Family : 가족상태 API,Res

--- family -FE/app/src/main/java/com/ssafy/family/data/remote/api/MainFamilyAPI.kt
+++ family -FE/app/src/main/java/com/ssafy/family/data/remote/api/MainFamilyAPI.kt
@@ -1,5 +1,6 @@
 package com.ssafy.family.data.remote.api
 
+import com.ssafy.family.data.remote.res.FamilyProfileRes
 import com.ssafy.family.data.remote.res.MissionRes
 import retrofit2.Response
 import retrofit2.http.GET
@@ -7,4 +8,7 @@ import retrofit2.http.GET
 interface MainFamilyAPI {
     @GET("/api/main/mission")
     suspend fun getTodayMission():Response<MissionRes>
+
+    @GET("/api/main/profileList")
+    suspend fun getFamilyProfileList():Response<FamilyProfileRes>
 }
---
+++ family -FE/app/src/main/java/com/ssafy/family/data/remote/res/FamilyProfile.kt
@@ -0,0 +1,20 @@
+package com.ssafy.family.data.remote.res
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class FamilyProfile(
+    @SerializedName("userPk")
+    val userPk: Int,
+    @SerializedName("imagePath")
+    val imagePath: String,
+    @SerializedName("role")
+    val role: String,
+    @SerializedName("emotion")
+    val emotion: String,
+    @SerializedName("comment")
+    val comment: String,
+
+    ): Parcelable
