a7c987072ae0ca062749cc2bee3230cb66d008cd
SuhyungLee <eel0511@gmail.com>
2022-05-10 16:05:49 +0900

    [S06P31D203-269] FIX/FE/채팅 : 채팅 FCM

--- BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/controller/ChattingController.java
+++ BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/controller/ChattingController.java
@@ -41,6 +41,11 @@ public class ChattingController {
 
         return responseService.getListResult(profileService.getProfileListByUserPk(userPk));
     }
+
+    @Operation(summary = "채팅 맴버들 프로필 조회", description = "<strong>채팅 맴버들<strong>의 프로필을 조회한다.",
+            parameters = {
+                    @Parameter(name = "X-AUTH-TOKEN", description = "JWT Token", required = true, in = HEADER)
+            })
     @PostMapping(value = "/send")
     public CommonResult send(@RequestParam String text, Authentication authentication) throws  IOException {
         Long userPk = Long.parseLong(authentication.getName());
