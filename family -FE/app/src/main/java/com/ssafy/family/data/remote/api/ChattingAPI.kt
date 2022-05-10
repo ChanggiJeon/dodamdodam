7424ae0c4ba5d4b292513375d4dc9319e89de2c4
SuhyungLee <eel0511@gmail.com>
2022-05-10 15:46:08 +0900

    [S06P31D203-269] Feature/FE/채팅 : 채팅 FCM

--- family -FE/app/src/main/java/com/ssafy/family/data/remote/api/ChattingAPI.kt
+++ family -FE/app/src/main/java/com/ssafy/family/data/remote/api/ChattingAPI.kt
@@ -1,5 +1,6 @@
 package com.ssafy.family.data.remote.api
 
+import com.ssafy.family.config.BaseResponse
 import com.ssafy.family.data.remote.res.ChattingRes
 import retrofit2.Response
 import retrofit2.http.*
@@ -9,4 +10,6 @@ interface ChattingAPI {
     @GET("api/chatting")
     suspend fun getMember():Response<ChattingRes>
 
+    @POST("/api/chatting/send")
+    suspend fun sendChattingFCM(@Query("text")text:String):Response<BaseResponse>
 }
