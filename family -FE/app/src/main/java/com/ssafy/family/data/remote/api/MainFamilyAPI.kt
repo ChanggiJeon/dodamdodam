bb6b669cf68f4d2a4e8c88fdf9304edfd65e6299
SuhyungLee <eel0511@gmail.com>
2022-05-10 14:30:00 +0900

    [S06P31D203-268] Feature/FE/family : 푸시 api, req

--- family -FE/app/src/main/java/com/ssafy/family/data/remote/api/MainFamilyAPI.kt
+++ family -FE/app/src/main/java/com/ssafy/family/data/remote/api/MainFamilyAPI.kt
@@ -1,9 +1,13 @@
 package com.ssafy.family.data.remote.api
 
+import com.ssafy.family.config.BaseResponse
+import com.ssafy.family.data.remote.req.SendPushReq
 import com.ssafy.family.data.remote.res.FamilyProfileRes
 import com.ssafy.family.data.remote.res.MissionRes
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface MainFamilyAPI {
     @GET("/api/main/mission")
@@ -11,4 +15,7 @@ interface MainFamilyAPI {
 
     @GET("/api/main/profileList")
     suspend fun getFamilyProfileList():Response<FamilyProfileRes>
+
+    @POST("/api/main/alarm")
+    suspend fun sendAlarm(@Body sendPushReq: SendPushReq):Response<BaseResponse>
 }
