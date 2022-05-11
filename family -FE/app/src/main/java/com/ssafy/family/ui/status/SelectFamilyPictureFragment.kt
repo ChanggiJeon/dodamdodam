f1a0aba6c0de17ef2ea49896ce3e756772c5b733
John Kim <dhkdhd123@naver.com>
2022-05-11 14:18:53 +0900

    Feat/FE/Add : edit family picture

--- family -FE/app/src/main/java/com/ssafy/family/ui/status/SelectFamilyPictureFragment.kt
+++ family -FE/app/src/main/java/com/ssafy/family/ui/status/SelectFamilyPictureFragment.kt
@@ -17,18 +17,21 @@ import com.bumptech.glide.Glide
 import com.ssafy.family.R
 import com.ssafy.family.databinding.FragmentSelectFamilyPictureBinding
 import com.ssafy.family.ui.Adapter.SinglePhotoRecyclerViewAdapter
+import com.ssafy.family.ui.startsetting.StartSettingActivity
+import com.ssafy.family.util.Constants
+import java.io.File
 
 class SelectFamilyPictureFragment : Fragment() {
     private lateinit var binding: FragmentSelectFamilyPictureBinding
     private lateinit var photoRecyclerViewAdapter: SinglePhotoRecyclerViewAdapter
     private val statusViewModel by activityViewModels<StatusViewModel>()
-    var selectedPicturePosition: Int? = null
+    var selectedPicturePosition: Int = -1
 
     private val itemClickListener = object : SinglePhotoRecyclerViewAdapter.ItemClickListener {
         override fun onClick(uri: Uri, view: View, position: Int) {
             if (selectedPicturePosition == position) {
-                statusViewModel.setImgUri(null)
-                selectedPicturePosition = null
+                statusViewModel.deleteImgUri()
+                selectedPicturePosition = -1
             } else {
                 statusViewModel.setImgUri(uri)
                 selectedPicturePosition = position
@@ -43,15 +46,26 @@ class SelectFamilyPictureFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSelectFamilyPictureBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // 하단 버튼 텍스트 설정
         binding.selectFamilyPictureButtonInclude.button.text = "취소"
         binding.selectFamilyPictureButtonInclude.button2.text = "완료"
+        // 하단 버튼 클릭리스너 등록
+        binding.selectFamilyPictureButtonInclude.button.setOnClickListener(View.OnClickListener {
+            // TODO : 가족사진 변경 취소 -> 설정 페이지로 이동
+        })
+        binding.selectFamilyPictureButtonInclude.button2.setOnClickListener(View.OnClickListener {
+            // 사진 변경 api 요청
+            val imageFile = statusViewModel.selectedImgUri.value
+            statusViewModel.editFamilyPicture(imageUriToFile(imageFile))
+        })
+        // 리사이클러뷰 설정
         photoRecyclerViewAdapter = SinglePhotoRecyclerViewAdapter().apply {
             itemClickListener = this@SelectFamilyPictureFragment.itemClickListener
         }
@@ -59,6 +73,7 @@ class SelectFamilyPictureFragment : Fragment() {
             adapter = photoRecyclerViewAdapter
             layoutManager = GridLayoutManager(requireActivity(), 4, RecyclerView.VERTICAL, false)
         }
+        // 선택된 이미지 Uri에 따른 상단 이미지뷰 UI 설정
         statusViewModel.selectedImgUri.observe(requireActivity()) {
             val imageView = binding.selectedFamilyPicture
             if (it == null) {
@@ -79,6 +94,7 @@ class SelectFamilyPictureFragment : Fragment() {
         photoRecyclerViewAdapter.notifyDataSetChanged()
     }
 
+    // 갤러리 이미지 가져오는 함수 : setImageUrisFromCursor(getPhotoCursor())
     fun setImageUrisFromCursor(cursor: Cursor): List<Uri> {
         val list = mutableListOf<Uri>()
         cursor.use {
@@ -94,7 +110,6 @@ class SelectFamilyPictureFragment : Fragment() {
         }
         return list
     }
-
     fun getPhotoCursor(): Cursor {
         val resolver = requireActivity().contentResolver
         var queryUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
@@ -110,6 +125,33 @@ class SelectFamilyPictureFragment : Fragment() {
 
         queryUri = queryUri.buildUpon().build()
         return resolver.query(queryUri, img, null, null, orderBy)!!
+    }
+
+    // 이미지 Uri -> File
+    private fun imageUriToFile(uri: Uri?): File? {
+        var uri: Uri? = uri
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        if (uri == null) {
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+        var cursor: Cursor? = (activity as StatusActivity).contentResolver.query(
+            uri!!,
+            projection,
+            null,
+            null,
+            MediaStore.Images.Media.DATE_MODIFIED + " desc"
+        )
+        if (cursor == null || cursor.columnCount < 1) {
+            return null
+        }
+        val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor.moveToFirst()
+        val path: String = cursor.getString(column_index)
+        if (cursor != null) {
+            cursor.close()
+            cursor = null
+        }
+        return File(path)
     }
 
 }
