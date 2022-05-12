ef3c90c3629b0c2656f898069fa965c8bb084bf7
John Kim <dhkdhd123@naver.com>
2022-05-12 13:55:50 +0900

    Bugfix/FE : home activity and logfin fragment

--- family -FE/app/src/main/java/com/ssafy/family/di/ApiModule.kt
+++ family -FE/app/src/main/java/com/ssafy/family/di/ApiModule.kt
@@ -126,6 +126,12 @@ object ApiModule {
     fun provideMainFamilyApiService(retrofit: Retrofit): MainFamilyAPI {
         return retrofit.create(MainFamilyAPI::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideWishtreeApiService(retrofit: Retrofit): WishtreeAPI {
+        return retrofit.create(WishtreeAPI::class.java)
+    }
 //    fun initRetrofit() {
 //        val client: OkHttpClient = OkHttpClient.Builder()
 //            .readTimeout(TIME_OUT, TimeUnit.MILLISECONDS)
