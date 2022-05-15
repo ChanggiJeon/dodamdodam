1912bb5d448c522999c9b7f025050d120ad6d16e
전창기 <wjs1724@naver.com>
2022-05-15 22:14:59 +0900

    Feat/BE/Refactor : Code Refactor Chatting, profile controllers, fcmService

--- BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/service/FcmService.java
+++ BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/service/FcmService.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.common.net.HttpHeaders;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
-import com.google.firebase.messaging.Message;
 import com.ssafy.core.dto.res.FcmMessageResDto;
 import com.ssafy.core.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
@@ -32,6 +31,7 @@ public class FcmService {
     private final ProfileRepository profileRepository;
 
     public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+
         String message = makeMessage(targetToken, title, body);
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
@@ -43,42 +43,21 @@ public class FcmService {
                 .build();
         Response response = client.newCall(request)
                 .execute();
-        System.out.println(response.body().string());
     }
-
-//    public void sendMessagesToMany(String[] targetToken, String title, String body) throws IOException {
-////        String token = "cMzARCj6hVZZjIXv-LZTsZ:APA91bEL0L-lJTP_MH9a_-IAptkIP-L7ERO3Yser0OwglCIIlTePEg5z9sTodOiBm8KA3QspkC-y_aEZ4BfR2oQ9nzkRmlG06hdzQ381nTMpeRm03quOPdhyoec8v_x3pcLN-pNl9Y91";
-//        String message = makeMessage(targetToken, title, body);
-//        OkHttpClient client = new OkHttpClient();
-//        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
-//        Request request = new Request.Builder()
-//                .url(fireBaseScope)
-//                .post(requestBody)
-//                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
-//                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
-//                .build();
-//        Response response = client.newCall(request)
-//                .execute();
-//        System.out.println(response.body().string());
-//    }
 
     private String makeMessage(String token, String title, String body)
             throws JsonProcessingException {
+
         FcmMessageResDto fcmMessage = FcmMessageResDto.builder()
                 .message(FcmMessageResDto.Message.builder()
                         .token(token)
-//                        .notification(FcmMessageResDto.Notification.builder()
-//                                .title(title)
-//                                .body(body)
-//                                .image(null)
-//                                .build()
-//                        )
                         .data(FcmMessageResDto.data.builder().body(body).title(title).build())
                         .build()
                 ).build();
 
         return objectMapper.writeValueAsString(fcmMessage);
     }
+
     private String getAccessToken() throws IOException {
 
         GoogleCredentials googleCredentials = GoogleCredentials
@@ -89,7 +68,10 @@ public class FcmService {
         return googleCredentials.getAccessToken().getTokenValue();
     }
 
+    @Nullable
+    @Transactional(readOnly = true)
     public String findFcmTokenByProfileId(Long profileId) {
+
         return profileRepository.findFcmTokenByProfileId(profileId);
     }
 }
