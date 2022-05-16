48b5dbb552495d90e58b58322589b06d05a9bec1
전창기 <wjs1724@naver.com>
2022-05-16 14:24:55 +0900

    [S06P31D203-174]Feat/BE/Refactor : Refactor UserService.java

--- BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/service/UserService.java
+++ BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/service/UserService.java
@@ -51,7 +51,7 @@ public class UserService {
     @Transactional(readOnly = true)
     public void checkExistId(String userId) {
 
-        if (userRepository.existsUserByUserId(userId)) {
+        if (Boolean.TRUE.equals(userRepository.existsUserByUserId(userId))) {
             throw new CustomException(ErrorCode.DUPLICATE_USER_ID);
         }
     }
@@ -197,11 +197,11 @@ public class UserService {
         user.updateRefreshToken(refreshToken);
         userRepository.save(user);
 
-        ProfileIdAndFamilyIdResDto Ids =
+        ProfileIdAndFamilyIdResDto ids =
                 profileRepository.findProfileIdAndFamilyIdByUserPk(user.getUserPk());
 
         //profile Id와 family Id는 둘 다 존재하거나, 둘다 없음.
-        if(Ids == null){
+        if(ids == null){
             return SignInResDto.builder()
                     .jwtToken(token)
                     .refreshToken(refreshToken)
@@ -210,8 +210,8 @@ public class UserService {
         }
 
         return SignInResDto.builder()
-                .profileId(Ids.getProfileId())
-                .familyId(Ids.getFamilyId())
+                .profileId(ids.getProfileId())
+                .familyId(ids.getFamilyId())
                 .jwtToken(token)
                 .refreshToken(refreshToken)
                 .name(user.getName())
