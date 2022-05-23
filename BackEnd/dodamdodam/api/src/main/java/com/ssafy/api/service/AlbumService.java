40cae1b0fb18f8697008db4ffcbd3ad81e7bc3c0
전창기 <wjs1724@naver.com>
2022-05-23 13:00:31 +0900

    Feat/BE/Fix : album update bug fix

--- BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/service/AlbumService.java
+++ BackEnd/dodamdodam/api/src/main/java/com/ssafy/api/service/AlbumService.java
@@ -271,22 +271,15 @@ public class AlbumService {
         //3. 삭제할 사진 삭제
         int[] deleteIndexList = albumUpdateReqDto.getPictureIdList();
 
-        if (deleteIndexList == null) {
-            List<Picture> pictureList = pictureRepository.findPictureListByAlbumId(album.getId());
-
-            System.out.println(deleteIndexList.length);
-            for(Picture p : pictureList){
-                System.out.println(p.getId());
-            }
-
-            System.out.println("어디까지 오는거야..");
-            for(int i=0; i<deleteIndexList.length; i++){
-                System.out.println(deleteIndexList[i]);
-            }
+        if (deleteIndexList != null) {
 
             List<Long> deleteIdList = new ArrayList<>();
-            for(int id : deleteIndexList){
-                deleteIdList.add(pictureList.get(id).getId());
+
+            System.out.println(deleteIndexList.length);
+
+            for(long id : deleteIndexList){
+                deleteIdList.add(id);
+                System.out.println(id);
             }
 
             List<Picture> deletePictureList =
